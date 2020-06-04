866b8414a1cbbc4a3004142c5c3bd9fc5599e8bd
Eric Badiere <ebadiere@gmail.com>
2020-06-03 21:26:17 -0600

    WIP. Working on send.

--- client/client.go
+++ client/client.go
@@ -2,8 +2,14 @@ package client
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
+	"math/big"
 )
 
 func main() {
@@ -20,4 +26,66 @@ func connect(rpcUrl string) (*ethclient.Client, context.Context) {
 	ctx := context.Background()
 
 	return client, ctx
+}
+
+func send(client *ethclient.Client, ctx context.Context, privateKey *ecdsa.PrivateKey, amount float64) {
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//gweiValue := new(big.Float).Mul(sAmount, big.NewFloat(math.Pow10(18))) // in wei (1 eth)
+	value := floatToBigInt(amount)
+	gasLimit := uint64(21000) // in units
+	//gasPrice := big.NewInt(30000000000) // in wei (30 gwei)
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	toAddress := common.HexToAddress("0xCE8504D1407CB8E7D406B2e0f9a43097e0F80699")
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, nil)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.SendTransaction(ctx, signedTx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", signedTx.Hash().Hex())
+
+}
+
+func floatToBigInt(val float64) *big.Int {
+	bigval := new(big.Float)
+	bigval.SetFloat64(val)
+	// Set precision if required.
+	// bigval.SetPrec(64)
+
+	coin := new(big.Float)
+	coin.SetInt(big.NewInt(1000000000000000000))
+
+	bigval.Mul(bigval, coin)
+
+	result := new(big.Int)
+	bigval.Int(result) // store converted number in result
+
+	return result
 }
--- client/client_test.go
+++ client/client_test.go
@@ -1,11 +1,29 @@
 package client
 
 import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
 	"testing"
 )
 
-func TestClientConnection(t *testing.T) {
+func TestSend(t *testing.T) {
 
+	fmt.Println("Hello")
+
+	client, ctx := getClientConnection(t)
+	privateKey, err := crypto.HexToECDSA("2dd5ee8e6cd77d432f803612b819dbb0e0447ff98a5d27996b6ce7e60dc09d3b")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	send(client, ctx, privateKey, 10)
+
+}
+
+func getClientConnection(t *testing.T) (*ethclient.Client, context.Context) {
 	rpcUrl := "http://127.0.0.1:7545"
 
 	client, ctx := connect(rpcUrl)
@@ -13,4 +31,6 @@ func TestClientConnection(t *testing.T) {
 	if chainID == nil {
 		t.Error("Connection Failed")
 	}
+
+	return client, ctx
 }
